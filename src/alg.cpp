c62f5fd5beac9522ade9f154c475432639c5f694
Yukineku <115874122+Yukineku@users.noreply.github.com>
2024-03-24 22:55:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,34 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int count = 0; // счётчик
+    int left = 0; // Левая граница интервала
+    int right = size - 1; // Правая граница интервала
+
+    // Пока левая граница не пересекла правую
+    while (left <= right) {
+        int mid = left + (right - left) / 2; // Находим середину интервала
+        if (arr[mid] == value) {
+            // Если значение найдено, увеличиваем счетчик
+            count++;
+            // Проверяем соседние элементы на предмет дополнительных вхождений
+            int leftFrMid = mid - 1;
+            int rightFrMid = mid + 1;
+            while (leftFrMid >= 0 && arr[leftFrMid] == value) {
+                count++;
+                leftFrMid--;
+            }
+            while (rightFrMid < size && arr[rightFrMid] == value) {
+                count++;
+                rightFrMid++;
+            }
+            break; // выход из цикла при нахождении всех элементов
+        } else if (arr[mid] < value) {//значение справа
+            left = mid + 1; // Сужаем интервал справа
+        } else {
+            right = mid - 1; // Сужаем интервал слева т.к значение слева
+        }
+    }
+    return count; // Возвращаем количество найденных элементов
+}
 }
